fc7e23b1d93a383a914e3d8cf701990f6e078711
Olivia Mack-Solden <omacksolden@gmail.com>
2016-07-30 16:16:55 -0400

    Add orange test icons to three boxes

--- index.php
+++ index.php
@@ -9,10 +9,15 @@
           </div>
           <section id="threebox">
             <h2>Your trustworthy auto body shop.</h2>
-            <p>We believe in helping our customers in any way we can.We hope
+            <p>We believe in helping our customers in any way we can. We hope
               you choose Slick's for any of your auto repair needs. Listed
               below are just a few types of repairs that we specialize in.
             </p>
+              <ul id="boxes">
+                <li><div class="orange"></div><h4 class="box-heading">COLLISION REPAIR</h4><p>We specialize in repairing damage done by an accident or other forms of impact.</p></li>
+                <li><div class="orange"></div><h4 class="box-heading">ENGINE REPAIR</h4><p>If you're having issues with your engine, we'll diagnose and repair it on the spot.</p></li>
+                <li><div class="orange"></div><h4 class="box-heading">INTERIOR REPAIR</h4><p>We can easily replace any of the interior parts that have damage or wear.</p></li>
+              </ul>
           </section>
         </div>
     </section>
