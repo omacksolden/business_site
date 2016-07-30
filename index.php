f877df484ba999d5211609272c520cd2a607b68b
Olivia Mack-Solden <omacksolden@gmail.com>
2016-07-30 14:57:04 -0400

    Add text content to the three boxes div

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
             <p>Experienced. Committed. Results Driven.</p>
             <button><a href="about">LEARN MORE</a></button>
           </div>
+          <section id="threebox">
+            <h2>Your trustworthy auto body shop.</h2>
+            <p>We believe in helping our customers in any way we can.We hope
+              you choose Slick's for any of your auto repair needs. Listed
+              below are just a few types of repairs that we specialize in.
+            </p>
+          </section>
         </div>
     </section>
 <?php get_footer(); ?>
